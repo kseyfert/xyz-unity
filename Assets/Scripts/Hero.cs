8d128d362aeef05a327fb33f1a36072a2b4c0b0a
Kesha Seyfert <sivsais@gmail.com>
2023-07-12 19:28:36 +0400

    movement

---
+++ Assets/Scripts/Hero.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hero : MonoBehaviour
+{
+    [SerializeField] private float speed = 1;
+    private Vector3 _direction;
+    
+    public void SetDirection(float directionX)
+    {
+        _direction = new Vector3(Math.Sign(directionX), 0, 0);
+    }
+
+    public void SetDirection(Vector2 direction)
+    {
+        _direction = new Vector3(Math.Sign(direction.x), Math.Sign(direction.y), 0);
+    }
+
+    private void Update()
+    {
+        transform.position += _direction * (speed * Time.deltaTime);
+    }
+}
