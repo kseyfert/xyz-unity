8441ff1bf51bba562e4a5ddc3aab7ed02ea503d2
Kesha Seyfert <sivsais@gmail.com>
2023-07-17 13:33:33 +0400

    +coin

---
+++ Assets/Scripts/PixelCrew/Components/DestroyObject.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace PixelCrew.Components
+{
+    public class DestroyObject : MonoBehaviour
+    {
+        [SerializeField] private GameObject target;
+        
+        public void DoDestroy()
+        {
+            Destroy(target);
+        }
+    }
+}
