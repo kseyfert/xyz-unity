4a368415b3ccbbee90f74770b823eb7c0db2369b
Kesha Seyfert <sivsais@gmail.com>
2023-10-16 18:18:05 +0200

    circle moving

--- Assets/Scripts/PixelCrew/Components/Movings/CircleMovingComponent.cs
+++ Assets/Scripts/PixelCrew/Components/Movings/CircleMovingComponent.cs
@@ -1,12 +1,41 @@
+using System;
+using UnityEditor;
 using UnityEngine;
 
 namespace PixelCrew.Components.Movings
 {
     public class CircleMovingComponent : AMovingComponent
     {
+        [SerializeField] private Vector2 center;
+
+        private float _radius;
+        private float _startAngle;
+
+        protected override void Start()
+        {
+            base.Start();
+            
+            _radius = ((Vector2)transform.position - center).magnitude;
+
+            var angle = Vector2.Angle(GetCurrent() - center, Vector2.right);
+            if (center.y >= GetCurrent().y) angle = -angle;
+            _startAngle = angle * Mathf.PI / 180;
+        }
+
         protected override Vector2 CalcNewPosition()
         {
-            throw new System.NotImplementedException();
+            var original = GetOriginal();
+            return new Vector2()
+            {
+                x = center.x + _radius * Mathf.Cos(_startAngle + GetTime()),
+                y = center.y + _radius * Mathf.Sin(_startAngle + GetTime())
+            };
+        }
+
+        private void OnDrawGizmosSelected()
+        {
+            Handles.color = new Color(0, 0, 1, 0.3f);
+            Handles.DrawSolidDisc(center, Vector3.forward, 0.1f);
         }
     }
 }
--- Assets/Scripts/PixelCrew/Components/Movings/SinMovingComponent.cs
+++ Assets/Scripts/PixelCrew/Components/Movings/SinMovingComponent.cs
@@ -1,4 +1,6 @@
+using System;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace PixelCrew.Components.Movings
 {
@@ -6,7 +8,16 @@ namespace PixelCrew.Components.Movings
     {
         [SerializeField] private float amplitude = 1f;
         [SerializeField] private float frequency = 1f;
+        [SerializeField] private bool randomized = false;
+
+        private float _seed;
         
+        protected override void Start()
+        {
+            base.Start();
+            if (randomized) _seed = Random.value * Mathf.PI * 2f;
+        }
+
         protected override Vector2 CalcNewPosition()
         {
             var original = GetOriginal();
@@ -18,7 +29,7 @@ namespace PixelCrew.Components.Movings
             var newPosition = new Vector2
             {
                 x = position.x + direction * speed,
-                y = original.y + amplitude * Mathf.Sin(frequency * time)
+                y = original.y + amplitude * Mathf.Sin(_seed + frequency * time)
             };
 
             return newPosition;
